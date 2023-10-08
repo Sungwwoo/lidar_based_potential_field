1d8652825b2225ff5b516a8a0bf009ed4f387584
Sungwwoo <p11120076@gmail.com>
2023-10-09 00:29:57 +0900

    .

--- scripts/main.py
+++ scripts/main.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python3
 
 import rospy
-from lidar_based_potential_field.basic_pf import BasicAPF
+from lidar_based_potential_field.potential_fields import BasicAPF
 import tf2_ros
 
 if __name__ == "__main__":
@@ -12,8 +12,7 @@ if __name__ == "__main__":
     apf.run()
     while not rospy.is_shutdown():
         forces = apf.get_forces()
-        rospy.loginfo("Forces: %f, %f ,%f", forces[0], forces[1], forces[2])
+        print(forces)
         weights = apf.get_weights()
         rospy.loginfo("Weights: %d, %d", weights[0], weights[1])
         apf.set_weights(20, 200)
-        
